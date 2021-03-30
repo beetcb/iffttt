0759797013c470663f0091fdce0d5adf2d805099
be beet <63141491+beetcb@users.noreply.github.com>
2021-03-30 19:46:53 +0800

    feat: format mail text content

--- api/iffttt.ts
+++ api/iffttt.ts
@@ -10,16 +10,21 @@ export default (request: VercelRequest, response: VercelResponse) => {
   } = request
 
   // Get filter and filter content
-  const regex = new RegExp(typeof filter === 'string' ? filter : '', 'i')
+  const regex = new RegExp(typeof filter === 'string' ? filter : '.*', 'i')
   const content: Content | null = body ? JSON.parse(body) : null
 
-  // Get filterd content
-  const filtered = regex.exec(content.text)
-  const filteredText = filtered ? filtered[0] : null
+  if (content) {
+    // Get filterd content
+    const filtered = regex.exec(content.text)
+    const filteredText = filtered ? filtered[0] : null
 
-  if (filteredText) {
-    mail('i@beetcb.com', 'iffttt -> SMS', filteredText).then((_) =>
-      console.log('success')
-    )
+    // Send mail
+    if (filteredText) {
+      // Format mail
+      const text = `SMS Content: ${filteredText}\nFrom: ${content.from}\nTrigger: ${content.occurred}`
+      mail('i@beetcb.com', 'iffttt -> SMS', text).then((_) =>
+        console.log('success')
+      )
+    }
   }
 }
